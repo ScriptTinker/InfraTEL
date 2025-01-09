c1cfdb5180c637b131749af7c948dbdd136f38b5
ScriptTinker <moraruedi11@outlook.com>
2025-01-09 13:15:47 +0000

    Fixed Layout

---
+++ InfraTEL_flask/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "136aba0cd611a36e9fbcfe5a2c695a88d4e89c33be32a6a0de2f8c2c7beb914e"
+
+from InfraTEL_flask import routes
